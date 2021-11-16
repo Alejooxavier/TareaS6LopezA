bc4d6890f3072d513c58f4e3520c42f8b18e57d4
AlejandroLopez <65260293+Alejooxavier@users.noreply.github.com>
2021-11-16 00:36:33 -0500

    Agregar archivos de proyecto.

---
+++ TareaS6LopezA/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaS6LopezA
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.1.8/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<TareaS6LopezA.WS.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+            
+        }
+
+        public async void get()
+        {
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<TareaS6LopezA.WS.Datos> posts = JsonConvert.DeserializeObject<List<TareaS6LopezA.WS.Datos>>(content);
+                _post = new ObservableCollection<TareaS6LopezA.WS.Datos>(posts);
+
+                MyListView.ItemsSource = _post;
+            }
+            catch (Exception ex){
+                await DisplayAlert("Error", "ERROR" + ex.Message, "OK");
+            }
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new viewInsertarA());
+
+        }
+    }
+}
